648b109193166a448c5e0825efdc622cbe6b3501
zhangsong <zhangsong.888@bytedance.com>
2021-04-07 14:05:43 +0800

    【master】新增-first commit

---
+++ app/src/main/java/com/imooc/imooc_music_app_test/TestActivity.java
@@ -0,0 +1,15 @@
+package com.imooc.imooc_music_app_test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class TestActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+    }
+}
